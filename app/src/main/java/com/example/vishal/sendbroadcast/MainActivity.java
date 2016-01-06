0648428cf11326ba3df4ac5e7b8391b8652919ef
VishalG6 <vishalg_94@hotmail.com>
2016-01-06 22:40:58 +1100

    Initial commit

---
+++ app/src/main/java/com/example/vishal/sendbroadcast/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.vishal.sendbroadcast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendOutBroadcast (View view) {
+        Intent i = new Intent();
+        i.setAction("com.example.vishal.sendbroadcast");
+        i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(i);
+    }
+
+}
